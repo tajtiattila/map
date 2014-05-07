b0ffc34e68dd71de9180800de85dc4f2b5c7d1bc
Attila Tajti <attila.tajti@xmta.eu>
2014-05-07 16:45:17 +0200

    pgsql works but sigterm is not handled

--- docker/postgis/run.sh
+++ docker/postgis/run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+DBNAME=gis
+
+PGVER=9.3
+CONFIG=main
+
 ASPG="su postgres"
-DATADIR="/var/lib/postgresql/9.3/main"
-CONF="/etc/postgresql/9.3/main/postgresql.conf"
-POSTGRES="/usr/lib/postgresql/9.3/bin/postgres"
-INITDB="/usr/lib/postgresql/9.3/bin/initdb"
-PGISCONTRIB="/usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql"
+DATADIR="/var/lib/postgresql/$PGVER/$CONFIG"
+CONFIGFILE="/etc/postgresql/$PGVER/$CONFIG/postgresql.conf"
+LOGFILE="/var/log/postgresql/postgresql-$PGVER-$CONFIG.log"
+PGCTL="/usr/lib/postgresql/$PGVER/bin/pg_ctl"
+INITDB="/usr/lib/postgresql/$PGVER/bin/initdb"
+PGISCONTRIB="/usr/share/postgresql/$PGVER/contrib/postgis-2.1"
 
 function initdb() {
 	# test if DATADIR is existent
@@ -18,40 +24,70 @@ function initdb() {
 	if [ ! "$(ls -A $DATADIR)" ]; then
 		echo "Initializing Postgres Database at $DATADIR"
 		chown -R postgres $DATADIR
-		$ASPG sh -c "$INITDB $DATADIR"
-		$ASPG sh -c "$POSTGRES --single -D $DATADIR -c config_file=$CONF" <<< "CREATE USER docker WITH SUPERUSER PASSWORD 'docker';"
+		$ASPG sh -c "$PGCTL -D $DATADIR initdb"
 	fi
 }
 
 function startdb() {
-	trap "echo \"Sending SIGTERM to postgres\"; killall -s SIGTERM postgres" SIGTERM
+	echo "Starting database"
+	if tty --silent; then
+		LOG="-l $LOGFILE"
+	else
+		LOG=
+	fi
+	$ASPG sh -c "$PGCTL -D $DATADIR $LOG -o '-c config_file=/etc/postgresql/9.3/main/postgresql.conf' start"
+	sleep 5
+}
 
-	$ASPG sh -c "$POSTGRES -D $DATADIR -c config_file=$CONF" &
+function stopdb() {
+	echo "Stopping database"
+	$ASPG sh -c "$PGCTL -D $DATADIR stop"
+}
+
+function createuser() {
+	#$ASPG sh -c "createuser -d -s -r -l docker"
+	#$ASPG sh -c "psql postgres" <<EOF
+#ALTER USER docker WITH ENCRYPTED PASSWORD 'docker';
+#EOF
 }
 
 function createdb() {
-	dbname=gis
-
-	# check if database exists
-	$ASPG -c 'psql -d '$dbname' -c '"''"' >/dev/null 2>&1' && return
+	# Create the docker user
+	$ASPG sh -c "psql -c " <<EOF
+CREATE USER docker WITH SUPERUSER PASSWORD 'docker';
+EOF
 
 	# Create the database
-	$ASPG postgres createdb -O docker $dbname
+	$ASPG sh -c "createdb -O docker $DBNAME"
 
 	# Install the Postgis schema
-	$ASPG psql -d $dbname -f $PGISCONTRIB/postgis.sql
+	psql -q -U docker -d $DBNAME -f $PGISCONTRIB/postgis.sql
+
+	# Enable ST_Transform() operations on geometries
+	psql -q -U docker -d $DBNAME -f $PGISCONTRIB/spatial_ref_sys.sql
 
 	# Set the correct table ownership
-	$ASPG psql -d $dbname -c 'ALTER TABLE geometry_columns OWNER TO "docker"; ALTER TABLE spatial_ref_sys OWNER TO "docker";'
+	psql -q -U docker -d $DBNAME <<EOF
+ALTER TABLE geometry_columns OWNER TO "docker";
+ALTER TABLE spatial_ref_sys OWNER TO "docker";
+EOF
 }
 
 initdb
 startdb
-createdb
 
-if ! tty --silent; then
-	wait $!
-else
+# check if database exists
+if ! $ASPG sh -c "psql -d $DBNAME -c '' >/dev/null 2>&1"; then
+	createuser
+	createdb
+fi
+
+if tty --silent; then
+	# Drop the user into the shell before exiting
 	bash
-	killall -s SIGTERM postgres
+	stopdb
+else
+	trap stopdb SIGTERM
+	# Wait forever
+	tail -f /dev/null
 fi
